fbbeddf0d03f997b352b2039c5ebc41d977a335a
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2025-02-10 19:34:10 +0200

    creating routes and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 // import Counter from "./features/counter/Counter";
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddPostForm from './features/posts/AddPostForm';
 import PostsList from './features/posts/PostsList';
+import Layout from './components/Layout';
+import SinglePostPage from './features/posts/SinglePostPage';
 
 function App() {
   return (
-    <main className="App">
-      <AddPostForm />
-      <PostsList />
-    </main>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+      
+        <Route index element={<PostsList />} />
+
+        <Route path='post'>
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+        </Route>
+
+      </Route>
+    </Routes>
   );
 }
 
